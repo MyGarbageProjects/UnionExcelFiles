a44f993e00e62611d8422fe2d3fc9751061acbe0
lif0 <kadi741@mail.ru>
2018-12-03 21:21:40 +0300

    фыв

--- UnionExcelFiles/UnionExcelFiles/Program.cs
+++ UnionExcelFiles/UnionExcelFiles/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing.Design;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -15,33 +16,50 @@ namespace UnionExcelFiles
     {
         static List<Excel> ExcelList = new List<Excel>();
         private static string[,] unionTables;
-        private static readonly string pathFolder = $"{AppDomain.CurrentDomain.BaseDirectory}Контакты с Caspian Agency";
+        private static string pathFolder = $"{AppDomain.CurrentDomain.BaseDirectory}";
         static void Main(string[] args)
         {
-            DirectoryInfo di = new DirectoryInfo(pathFolder);
             List<string[,]> _tmp = new List<string[,]>();
+            Console.Write("Введите название папки в которой находятся файлы:");
+            string folder = Console.ReadLine();
+            pathFolder += folder;
+            DirectoryInfo di = new DirectoryInfo(pathFolder);
+           
             //foreach (FileInfo file in di.GetFiles())
             //{
             //    Console.WriteLine(file.Name);
             //    ExcelList.Add(new Excel(file));
             //    _tmp.Add(ExcelList.Last().ExcelTable);
             //}
+
+            FileInfo[] fi = di.GetFiles();
+
+            fi = fi.OrderBy(x => int.Parse(x.Name.Remove(x.Name.LastIndexOf(")")).Remove(0, x.Name.IndexOf("(") + 1))).ToArray();
+
+
             for (int i = 0; i < di.GetFiles().Length; i++)
             {
-                FileInfo file = new FileInfo($"{pathFolder}\\amocrm__contacts ({i}).xlsx");
-                Console.WriteLine(file.Name);
-                ExcelList.Add(new Excel(file));
+                //FileInfo file = new FileInfo($"{pathFolder}\\amocrm__contacts ({i}).xlsx");
+                Console.WriteLine(fi[i].Name);
+                ExcelList.Add(new Excel(fi[i]));
                 _tmp.Add(ExcelList.Last().ExcelTable);
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
+            if (_tmp.Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Папка пуста");
+                Console.ResetColor();
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Важно отметить, что начиная со 2го файла, 1я строка в документе пропускается.\r\nСчитается что там описание полей.");
             Console.ResetColor();
             Console.WriteLine("Порядок объединения соблюден?\r\nЕсли нет - просто закройте программу\r\nЕсли да - нажмите на любую клавишу");
             Console.ReadKey();
             int cursorRowIndex = 0;
             int offsetRow = 0;
-
+            Console.Write("Введите длинну пропуска между файлами(Обычно 1 достаточно):");
+            int offsetRowBeetwen = int.Parse(Console.ReadLine());
 
             using (OfficeOpenXml.ExcelPackage eP = new OfficeOpenXml.ExcelPackage())
             {
@@ -51,19 +69,19 @@ namespace UnionExcelFiles
 
                     for (int i = 0; i < ExcelList.Count; i++)
                     {
-                        for (int row = 1; row < ExcelList[i].ExcelTable.GetLength(0); row++)
+                        for (int row = 1; row < ExcelList[i].ExcelTable.GetLength(0)+1; row++)
                         {
                             if (i != 0 && row == 1)
                             {
                                 row++;
                             }
-                            for (int column = 1; column < ExcelList[i].ExcelTable.GetLength(1); column++)
+                            for (int column = 1; column < ExcelList[i].ExcelTable.GetLength(1)+1; column++)
                             {
                                 eWs.Cells[row + offsetRow, column].Value = ExcelList[i].ExcelTable[row - 1, column - 1];
                             }
                         }
 
-                        offsetRow += ExcelList[i].ExcelTable.GetLength(0)-2;//-2 чтоб пропустить 2 пустые строки, чтоб инфо шло друг-за-другом
+                        offsetRow += ExcelList[i].ExcelTable.GetLength(0) - offsetRowBeetwen;//-2 чтоб пропустить 2 пустые строки, чтоб инфо шло друг-за-другом
                     }
 
                     string nameUnionExcel = "UnionExcel.xlsx";
